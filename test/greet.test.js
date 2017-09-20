644ae8a265037b82631fffc3a275c798976987f6
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-09-19 21:00:49 -0700

    greet test working

---
+++ test/greet.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const greet = require('../lib/greet.js');
+const expect = require('expect');
+
+describe('Greet', function(){
+
+  it('should return hello + argument when called', function(){
+
+    expect(greet('world')).toEqual('hello world');
+
+  });
+});
+
+
+describe('Greet', function(){
+
+  it('should return null when the argument isnt a string', function(){
+
+    expect(greet(69)).toBe(null);
+
+  });
+});
